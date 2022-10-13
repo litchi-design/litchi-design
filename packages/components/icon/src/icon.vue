0333577d3eda771df62cc364d7748059b3ba99c4
coder-fang <1846015350@qq.com>
2022-10-13 13:53:44 +0800

    feat: init

---
+++ packages/components/icon/src/icon.vue
@@ -0,0 +1,28 @@
+<template>
+  <i :class="bem.b()" :style="style">
+    <slot></slot>
+  </i>
+</template>
+
+<script lang="ts" setup>
+import { computed, CSSProperties } from "vue";
+import { createNamespace } from "../../../utils/create";
+import { iconProps } from "./icon";
+const bem = createNamespace("icon");
+
+defineOptions({
+  name: "ZIcon",
+});
+
+const props = defineProps(iconProps);
+
+const style = computed<CSSProperties>(() => {
+  if (!props.color && !props.size) {
+    return {};
+  }
+  return {
+    ...(props.size ? { "font-size": props.size + "px" } : {}),
+    ...(props.color ? { color: props.color } : {}),
+  };
+});
+</script>
---
+++ test/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import ZIcon from "@rabbit-design/components/icon";
+import { AccessibilityOutline } from "@vicons/ionicons5";
+</script>
+
+<template>
+  <div>
+    <ZIcon>
+      <AccessibilityOutline></AccessibilityOutline>
+    </ZIcon>
+  </div>
+</template>
