9d96182d3a4d38942531bba2d8dd966a1bfb81a1
coder-fang <1846015350@qq.com>
2022-10-15 13:19:46 +0800

    feat: init design

--- packages/components/icon/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare const Icon: import("@litchi-design/utils/withInstall").withInstallSFC<import("vue").DefineComponent<{}, {}, any, import("vue").ComputedOptions, import("vue").MethodOptions, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, {}, string, import("vue").VNodeProps & import("vue").AllowedComponentProps & import("vue").ComponentCustomProps, Readonly<import("vue").ExtractPropTypes<{}>>, {}>>;
-export default Icon;
-export type { IconProps } from './src/icon';
-declare module 'vue' {
-    interface GlobalComponents {
-        LIcon: typeof Icon;
-    }
-}
--- packages/components/icon/index.js
+++
@@ -1,5 +0,0 @@
-import _Icon from './src/icon.vue';
-import withInstall from '@litchi-design/utils/withInstall';
-const Icon = withInstall(_Icon);
-export default Icon;
-//# sourceMappingURL=index.js.map
